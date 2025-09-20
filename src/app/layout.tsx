b4b22d54a65608b10b3c4becb23c0d32b01ec61c
Jorge Gonçalves de Oliveira <jorge.gdoliveira8@gmail.com>
2025-09-20 19:31:10 +0000

    cade o resto?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.React.Node;
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
